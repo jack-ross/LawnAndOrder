dfe02d784580c17a54a17db64ec48c17baf57808
jack-ross <jack@pickupguitarclub.com>
2016-12-01 09:57:07 -0500

    Working PubSub Modeo

--- Navigation-Server/navigation-server/index.js
+++ Navigation-Server/navigation-server/index.js
@@ -10,11 +10,13 @@ const redis = require("redis")
   , subscriber = redis.createClient(redisConfig)
   , publisher  = redis.createClient(redisConfig);
 
+const openCvChannel = "cv-channel";
+
 subscriber.on("message", function(channel, message) {
   console.log("Message '" + message + "' on channel '" + channel + "' arrived!")
 });
 
-subscriber.subscribe("test");
+subscriber.subscribe(openCvChannel);
 
 function calculateAction() {
     
