f9a1d834cd22d394750ef716061a7a3cb30dd739
Jack Ross <jross2112@gmail.com>
2017-02-15 14:37:59 -0500

    Updated MQTT

--- Embedded-Robot/beaglebone/mqttHandler.js
+++ Embedded-Robot/beaglebone/mqttHandler.js
@@ -1,17 +1,18 @@
 "use strict";
 
 const constants = require("./constants.js");
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://192.168.1.111:1883');
+
+const ROBOT_TOPIC = "robot-1";
  
 client.on('connect', function () {
-  client.subscribe('presence')
-  client.publish('presence', 'Hello mqtt')
+  client.subscribe(ROBOT_TOPIC);
+  client.publish('status', ROBOT_TOPIC + " connected!");
 })
  
 client.on('message', function (topic, message) {
   // message is Buffer 
-  console.log(message.toString())
-  console.log(message.toString() + " " + constants.ClientId)
-  client.end()
+  console.log(message.toString());
+  client.end();
 })
