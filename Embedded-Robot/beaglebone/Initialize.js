921b6c623615560312181a49d80b9f6fd63a9e08
parsonsbrett0 <parsonsbrett0@gmail.com>
2017-02-15 14:37:15 -0500

    Update Initialize.js

--- Embedded-Robot/beaglebone/Initialize.js
+++ Embedded-Robot/beaglebone/Initialize.js
@@ -8,32 +8,9 @@ var ENCODER_BL = "P8_18";
 var motorL_forward = "P9_34";
 var motorL_reverse = "P9_36";
 
+// const int anPin1 = 6; // Left Sensor
+var anPin1 = "P9_38";
+// const int anPin2 = 24; // Right Sensor
+var anPin2 = "P9_40";
 
-
-// encoder and motor variables have been changed to the correct pins
-
-// Generic variables
-int i = 0;
-int n = 8;
 boolean objDetected = false;
-int prev = 0;
-
-// variables for sensor 1
-//U2
-const int anPin1 = 6; // Left Sensor
-long distance1;
-int sensorBuffer1[8];
-float runningSum1;
-float runningAverage1;
-float inputValue1;
-float var1 = 0; //variance
-
-// variables for sensor 2
-//U6
-const int anPin2 = 24; // Right Sensor
-long distance2;
-int sensorBuffer2[8];
-float runningSum2;
-float runningAverage2;
-float inputValue2;
-float var2 = 0; //varience
